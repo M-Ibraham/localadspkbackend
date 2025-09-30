864237b5934438fced560a695df2a5ea96a14a64
M-Ibraham <ibrahim28077@gmail.com>
2025-10-01 00:25:46 +0500

    server port

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,10 @@ app.use("/api/campaigns", campaignRoutes);
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
 });
